96a38e10e6b72675443512470b9bb04ed8df641c
Bilous-Oleh <2005net2005@gmail.com>
2023-05-11 23:33:15 +0300

    Fix bugs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -65,7 +65,6 @@ export class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.contacts !== this.state.contacts) {
-      console.log('this.state.contacts');
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -48,7 +48,7 @@ export default class PhonebookForm extends Component {
             type="text"
             name="name"
             id={this.nameId}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={this.state.name}
             onChange={this.handleChange}
@@ -63,7 +63,7 @@ export default class PhonebookForm extends Component {
             type="tel"
             name="number"
             id={this.telId}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             value={this.state.number}
             onChange={this.handleChange}
